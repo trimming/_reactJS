8e9768ff669f73030167aba220168c371bf24ebe
Alexey Golikov <lexgolikoff@gmail.com>
2023-04-06 20:41:00 +0300

    create project to REACT

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
-import { Message } from "./components/Message";
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
-const messageText = 'My First React App';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript 1', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'}
+  ]);
 
+       
   return (
     <div className="App">
-      <Message text={messageText} />
+        <form>
+          <input type="text" placeholder="Название поста"/>
+          <input type="text" placeholder="Описание поста"/>
+          <MyButton>
+            Создать пост
+          </MyButton>
+        </form>
+        <PostList posts={posts} title={"Посты про JS"}/>                         
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export const PostItem = (props) => {
+    
+  return (
+    <div className="post">
+        <div className="post__component">
+            <strong>{props.post.id}.{props.post.title}</strong>
+            <div>
+                {props.post.body}
+            </div>
+        </div>
+        <div className="post__btns">
+            <button>Удалить</button>
+        </div>
+  </div>
+  );
+};
+
+
+export default PostItem;
